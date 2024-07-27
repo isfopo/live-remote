562bc494f59202d6742579476504a155cbfbe4df
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-27 18:25:34 -0400

    📝 (App.svelte): Refactor variable names and types for clarity and consistency   ✨ (App.svelte): Improve WebSocket handling and message sending functionality

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,47 +1,48 @@
 <script lang="ts">
   import { writable } from "svelte/store";
+  import { IncomingMessage, Method, OutgoingMessage } from "./types/Message";
 
   type State = {
-    requests: Array<Request>;
-    ws: WebSocket | null; // Store WebSocket instance
+    messages: Array<IncomingMessage>;
+    socket: WebSocket | null; // Store WebSocket instance
   };
 
   // Create a new store with the given data.
   export const state = writable<State>({
-    requests: [],
-    ws: null, // Initialize WebSocket to null
+    messages: [],
+    socket: null, // Initialize WebSocket to null
   });
 
   export const connect = () => {
     // Create a new websocket
-    const ws = new WebSocket("ws://localhost:8000");
+    const socket = new WebSocket("ws://localhost:8000");
 
-    ws.addEventListener("message", async (message: any) => {
+    socket.addEventListener("message", async (message: any) => {
       // Parse the incoming message here
-      const data: Request = JSON.parse(message.data);
+      const data: IncomingMessage = JSON.parse(message.data);
       // Update the state.  That's literally it.  This can happen from anywhere:
       // we're not in a component, and there's no nested context.
 
       state.update((state) => ({
         ...state,
-        requests: [data].concat(state.requests),
+        messages: [data].concat(state.messages),
       }));
     });
 
     // Store the WebSocket instance in the state
     state.update((prevState) => ({
       ...prevState,
-      ws: ws,
+      socket,
     }));
   };
 
-  export const send = () => {
+  export const send = (message: OutgoingMessage) => {
     // Get the current state
     state.update((prevState) => {
       // If the WebSocket is connected, send a message
-      const ws = prevState.ws;
-      if (ws && ws.readyState === WebSocket.OPEN) {
-        ws.send("hello ableton");
+      const socket = prevState.socket;
+      if (socket && socket.readyState === WebSocket.OPEN) {
+        socket.send(JSON.stringify(message));
       }
       return prevState;
     });
@@ -50,14 +51,20 @@
 
 <main>
   <h1>Hello, this is Live Remote!</h1>
-  {#if $state.ws && $state.ws.readyState === WebSocket.OPEN}
-    <button on:click={send}>Send</button>
+  {#if $state.socket && $state.socket.readyState === WebSocket.OPEN}
+    <button
+      on:click={() =>
+        send({
+          method: Method.SET,
+          address: "song",
+          prop: "is_playing",
+          value: 1,
+          type: "int",
+        })}>Play</button
+    >
   {:else}
     <button on:click={connect}>Connect</button>
   {/if}
-  {#each $state.requests as request}
-    <p>{request.url}</p>
-  {/each}
 </main>
 
 <style>
