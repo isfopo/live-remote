0c3fd50d74a9a4c593f33c6a9998a8c5b6c3b2db
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-27 02:17:19 -0400

    moves scripts to package.json

--- runners/install.py
+++ runners/install.py
@@ -1,29 +1,29 @@
 """
-  This script is designed to "install" your Remote Script to Ableton Live.
-  Essentially all that is to move the folder names 'src' to the appropriate
-  location on your computer for Live to recognize and compile it to be
-  used in a Live set.
+This script is designed to "install" your Remote Script to Ableton Live.
+Essentially all that is to move the folder names 'src' to the appropriate
+location on your computer for Live to recognize and compile it to be
+used in a Live set.
 
-  Once this script has run, start or restart Ableton and your Remote Script
-  should be available to you in your Midi preferences as one of the options
-  for a control surface. This process will have to be run again when you
-  have made changes in your code to see those changes in your control surface.
+Once this script has run, start or restart Ableton and your Remote Script
+should be available to you in your Midi preferences as one of the options
+for a control surface. This process will have to be run again when you
+have made changes in your code to see those changes in your control surface.
 
-  This script only works for Live 11 and 12 because it relocates the folder to the
-  User Library. If your User Library is in a different location than the
-  standard installation you will need to either need to change the path in
-  the script or pass in the path to your User Library as an argument using
-  the `--path` flag. Pass in the full path, including "User Library".
+This script only works for Live 11 and 12 because it relocates the folder to the
+User Library. If your User Library is in a different location than the
+standard installation you will need to either need to change the path in
+the script or pass in the path to your User Library as an argument using
+the `--path` flag. Pass in the full path, including "User Library".
 
-  This script also takes a guess at your username for the path. If this guess
-  is incorrect or there is some kind of error, try adding your username with
-  the `--user` flag.
+This script also takes a guess at your username for the path. If this guess
+is incorrect or there is some kind of error, try adding your username with
+the `--user` flag.
 
-  The `--name` flag can be used if you would like the name of the output folder
-  to be different than the name of the directory the project is in.
+The `--name` flag can be used if you would like the name of the output folder
+to be different than the name of the directory the project is in.
 
-  If you have changed the name of your src folder, then it will need to be
-  changed here as well.
+If you have changed the name of your src folder, then it will need to be
+changed here as well.
 """
 
 import argparse
@@ -43,7 +43,7 @@ parser.add_argument("--name", "-name of output folder", required=False)
 args = parser.parse_args()
 
 currentDir = os.getcwd()
-srcDir = os.path.join(currentDir, "script")
+srcDir = os.path.join(currentDir, "src")
 
 user = args.user or getpass.getuser()
 
