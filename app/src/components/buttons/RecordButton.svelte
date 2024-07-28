326651e696e14cdb07339c7558044687480b0d83
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-28 08:53:39 -0400

    ✨ Add RecordButton and StopButton components to provide functionality for recording and stopping in the Remote view for the application.

---
+++ app/src/components/buttons/RecordButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import IconButton from "./IconButton.svelte";
+  import { state } from "../../state";
+  import { FaSolidCircle } from "svelte-icons-pack/fa";
+  import { Method } from "../../types/Message";
+</script>
+
+<IconButton
+  icon={FaSolidCircle}
+  onClick={() =>
+    $state.send({
+      method: Method.SET,
+      address: "song",
+      prop: "record_mode",
+      value: 1,
+      type: "int",
+    })}
+  color={$state.live.song.record_mode ? "#dc143c" : "#000"}
+/>
---
+++ app/src/components/buttons/StopButton.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import IconButton from "./IconButton.svelte";
+  import { state } from "../../state";
+  import { FaSolidStop } from "svelte-icons-pack/fa";
+  import { Method } from "../../types/Message";
+</script>
+
+<IconButton
+  icon={FaSolidStop}
+  onClick={() =>
+    $state.send({
+      method: Method.SET,
+      address: "song",
+      prop: "is_playing",
+      value: 0,
+      type: "int",
+    })}
+/>
