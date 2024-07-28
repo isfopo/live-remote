9813a1c4c03a25faae3ebe32f8d66d2b4d3e7c29
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-28 08:47:59 -0400

    ✨ Remove unused imports from Remote.svelte to keep code clean and improve readability.

--- app/src/views/Remote.svelte
+++ app/src/views/Remote.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
   import PlayButton from "../components/buttons/PlayButton.svelte";
-  import { state } from "../state";
-  import { Method } from "../types/Message";
 </script>
 
 <PlayButton />
