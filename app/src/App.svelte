d57dd98fafe43b53aad0a470642efe57b04c4127
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-29 23:52:54 -0400

    ✨ (App.svelte): Add theme support with ThemeContext and ThemeToggle component 📝 (ThemeContext.svelte): Create ThemeContext component to manage theme state and colors 📝 (ThemeToggle.svelte): Create ThemeToggle component to toggle between light and dark themes 📝 (theme.ts): Define light and dark themes with corresponding colors

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
+  import ThemeToggle from "./components/toggle/ThemeToggle.svelte";
+  import ThemeContext from "./context/ThemeContext.svelte";
   import { state } from "./state";
   import Connect from "./views/Connect.svelte";
   import Remote from "./views/Remote.svelte";
 </script>
 
 <main>
-  {#if $state.socket && $state.socket.readyState === WebSocket.OPEN}
-    <Remote />
-  {:else}
-    <Connect />
-  {/if}
+  <ThemeContext>
+    <ThemeToggle />
+    {#if $state.socket && $state.socket.readyState === WebSocket.OPEN}
+      <Remote />
+    {:else}
+      <Connect />
+    {/if}
+  </ThemeContext>
 </main>
 
 <style>
   main {
     text-align: center;
+    background-color: var(--theme-background);
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
---
+++ app/src/theme.ts
@@ -0,0 +1,18 @@
+export const themes = [
+  {
+    name: "light",
+    colors: {
+      text: "#282230",
+      foreground: "#282230",
+      background: "#f1f1f1",
+    },
+  },
+  {
+    name: "dark",
+    colors: {
+      text: "#f1f1f1",
+      foreground: "#f1f1f1",
+      background: "#27323a",
+    },
+  },
+];
