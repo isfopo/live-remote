1dbc27b5a7d626ed822547a99e32570d1fef7bfd
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-31 10:19:15 -0400

    ✨ (theme.ts): introduce new theme color properties (play and record) to enhance the customization of the themes provided in the application.

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -1,20 +1,25 @@
 export type ThemeNames = "light" | "dark";
 
-export interface Theme {
-  text: string;
-  foreground: string;
-  background: string;
-}
+export type ThemeColors =
+  | "text"
+  | "foreground"
+  | "background"
+  | "play"
+  | "record";
 
-export const themes: Record<ThemeNames, Theme> = {
+export const themes: Record<ThemeNames, Record<ThemeColors, string>> = {
   light: {
     text: "#282230",
     foreground: "#282230",
     background: "#f1f1f1",
+    play: "#0B6623",
+    record: "#ff0000",
   },
   dark: {
     text: "#f1f1f1",
     foreground: "#f1f1f1",
     background: "#27323a",
+    play: "#0B6623",
+    record: "#ff0000",
   },
 };
