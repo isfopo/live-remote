ea27f4c3b605b9c0532895199677099ac901a138
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-30 23:01:59 -0400

    📝 (ThemeContext.svelte): Save the current theme to localStorage when toggling themes for persistence across sessions   📝 (ThemeContext.svelte): Load theme from localStorage when the app mounts or fallback to prefers-color-scheme if no theme is saved

--- app/src/context/ThemeContext.svelte
+++ app/src/context/ThemeContext.svelte
@@ -3,48 +3,47 @@
   import { writable } from "svelte/store";
   import { themes as _themes } from "../theme";
 
-  onMount(() => {
-    const userTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light";
-    const defaultTheme = getCurrentTheme(userTheme) || themes[0]; // Fallback to first theme if not found
-    _current = defaultTheme.name;
-    Theme.set(defaultTheme); // Initialize Theme store with the default theme
-    setRootColors(defaultTheme); // Set CSS vars to the default theme on mount
-  });
-
-  // expose props for customization and set default values
   export let themes = [..._themes];
-  // set state of current theme's name
   let _current = themes[0].name;
 
-  // utility to get current theme from name
   const getCurrentTheme = (name) => themes.find((h) => h.name === name);
-  // set up Theme store, holding current theme object
   const Theme = writable(getCurrentTheme(_current));
 
   setContext("theme", {
-    // providing Theme store through context makes store readonly
     theme: Theme,
     toggle: () => {
-      // update internal state
       let _currentIndex = themes.findIndex((h) => h.name === _current);
       _current =
         themes[_currentIndex === themes.length - 1 ? 0 : (_currentIndex += 1)]
           .name;
-      // update Theme store
       Theme.update((t) => ({ ...t, ...getCurrentTheme(_current) }));
       setRootColors(getCurrentTheme(_current));
+
+      // Save the current theme to localStorage
+      localStorage.setItem("currentTheme", _current);
     },
   });
 
   onMount(() => {
-    // set CSS vars on mount
+    // Load theme from localStorage or fallback to prefers-color-scheme
+    const savedTheme = localStorage.getItem("currentTheme");
+    if (savedTheme && getCurrentTheme(savedTheme)) {
+      _current = savedTheme;
+    } else {
+      const defaultTheme =
+        getCurrentTheme(
+          window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light"
+        ) || themes[0];
+
+      _current = defaultTheme.name;
+    }
+
+    Theme.set(getCurrentTheme(_current));
     setRootColors(getCurrentTheme(_current));
   });
 
-  // sets CSS vars for easy use in components
-  // ex: var(--theme-background)
   const setRootColors = (theme) => {
     for (let [prop, color] of Object.entries(theme.colors)) {
       let varString = `--theme-${prop}`;
