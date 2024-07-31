9521c21b51a42f42820c69d499a9976570a80516
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-31 09:14:36 -0400

    ♻️ (styles.ts): refactor setRootColors function to remove unnecessary variable assignment and improve code readability

--- app/src/helpers/styles.ts
+++ app/src/helpers/styles.ts
@@ -2,8 +2,7 @@ import { themes, type ThemeNames } from "../theme";
 
 export const setRootColors = (theme: ThemeNames) => {
   for (let [prop, color] of Object.entries(themes[theme])) {
-    let varString = `--theme-${prop}`;
-    document.documentElement.style.setProperty(varString, color);
+    document.documentElement.style.setProperty(`--theme-${prop}`, color);
   }
   document.documentElement.style.setProperty("--theme-name", theme);
 };
