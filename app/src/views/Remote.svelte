a02a3dc36b518760c8e22844a2402baa41e05b53
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-31 23:54:46 -0400

    ✨ (Remote.svelte): restructure layout using Svelte Grid Extended library for better component organization ⬆️ (package-lock.json): update svelte-grid-extended dependency to version 1.2.1 ⬆️ (package.json): add svelte-grid-extended as a dependency to the project

--- app/src/views/Remote.svelte
+++ app/src/views/Remote.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-  import PlayButton from "../components/buttons/PlayButton.svelte";
-  import RecordButton from "../components/buttons/RecordButton.svelte";
-  import StopButton from "../components/buttons/StopButton.svelte";
+  import Grid, { GridItem } from "svelte-grid-extended";
+
+  const items = [
+    { id: "0", x: 0, y: 0, w: 2, h: 5 },
+    { id: "1", x: 2, y: 2, w: 2, h: 2 },
+    { id: "2", x: 2, y: 0, w: 1, h: 2 },
+    { id: "3", x: 3, y: 0, w: 2, h: 2 },
+    { id: "4", x: 4, y: 2, w: 1, h: 3 },
+    { id: "5", x: 8, y: 0, w: 2, h: 8 },
+  ];
+
+  const itemSize = { height: 40 };
 </script>
 
-<PlayButton />
-<StopButton />
-<RecordButton />
+<Grid {itemSize} cols={10} collision="none">
+  {#each items as item}
+    <GridItem x={item.x} y={item.y} w={item.w} h={item.h}>
+      <div class="item">{item.id}</div>
+    </GridItem>
+  {/each}
+</Grid>
