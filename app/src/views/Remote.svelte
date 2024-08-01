1bccb29f26ba3158ea98fbb37269ce751011ebdc
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-01 01:01:43 -0400

    ✨ (Remote.svelte): Add TransportWidget to the Remote view to display transport controls   📝 (Remote.svelte): Update items array in Grid component to include only the TransportWidget   📝 (Remote.svelte): Add widgets object to map widget ids to their respective component in the Remote view   ✨ (TransportWidget.svelte): Create TransportWidget component with PlayButton, StopButton, and RecordButton components

--- app/src/views/Remote.svelte
+++ app/src/views/Remote.svelte
@@ -1,22 +1,20 @@
 <script lang="ts">
   import Grid, { GridItem } from "svelte-grid-extended";
+  import TransportWidget from "../widgets/TransportWidget.svelte";
 
-  const items = [
-    { id: "0", x: 0, y: 0, w: 2, h: 5 },
-    { id: "1", x: 2, y: 2, w: 2, h: 2 },
-    { id: "2", x: 2, y: 0, w: 1, h: 2 },
-    { id: "3", x: 3, y: 0, w: 2, h: 2 },
-    { id: "4", x: 4, y: 2, w: 1, h: 3 },
-    { id: "5", x: 8, y: 0, w: 2, h: 8 },
-  ];
+  const items = [{ id: "transport-widget", x: 0, y: 0, w: 2, h: 5 }];
 
   const itemSize = { height: 40 };
+
+  const widgets: Record<string, any> = {
+    "transport-widget": TransportWidget,
+  };
 </script>
 
 <Grid {itemSize} cols={10} collision="none">
-  {#each items as item}
-    <GridItem x={item.x} y={item.y} w={item.w} h={item.h}>
-      <div class="item">{item.id}</div>
+  {#each items as { id, x, y, w, h }}
+    <GridItem {x} {y} {w} {h} resizable={false}>
+      <svelte:component this={widgets[id]} />
     </GridItem>
   {/each}
 </Grid>
---
+++ app/src/widgets/TransportWidget.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import PlayButton from "../components/buttons/PlayButton.svelte";
+  import RecordButton from "../components/buttons/RecordButton.svelte";
+  import StopButton from "../components/buttons/StopButton.svelte";
+</script>
+
+<PlayButton />
+<StopButton />
+<RecordButton />
