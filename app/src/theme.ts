0479e7d725d6e0bb01eb0f03c966bcd435632f6a
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-31 22:37:37 -0400

    ✨ (theme.ts): define HexColor type to represent a hex color value with a # prefix   ♻️ (theme.ts): update themes object to use HexColor type for play color property to improve code readability

--- app/src/theme.ts
+++ app/src/theme.ts
@@ -1,4 +1,5 @@
 export type ThemeNames = "light" | "dark";
+export type HexColor = `#${string}`;
 
 export type ThemeColors =
   | "text"
@@ -7,19 +8,19 @@ export type ThemeColors =
   | "play"
   | "record";
 
-export const themes: Record<ThemeNames, Record<ThemeColors, string>> = {
+export const themes: Record<ThemeNames, Record<ThemeColors, HexColor>> = {
   light: {
     text: "#282230",
     foreground: "#282230",
     background: "#f1f1f1",
-    play: "#0B6623",
+    play: "#006600",
     record: "#ff0000",
   },
   dark: {
     text: "#f1f1f1",
     foreground: "#f1f1f1",
     background: "#27323a",
-    play: "#0B6623",
+    play: "#006600",
     record: "#ff0000",
   },
 };
