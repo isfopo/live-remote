1564c2ed3c522fe921567975c991f669d3c0c74a
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-03 08:15:15 -0400

    ✨ (AddButton.svelte): create a new Svelte component for an add button with a solid plus icon and a title of "Add", utilizing IconButton component and FaSolidPlus icon from svelte-icons-pack/fa

---
+++ app/src/components/buttons/AddButton.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import IconButton from "./IconButton.svelte";
+  import { FaSolidPlus } from "svelte-icons-pack/fa";
+</script>
+
+<IconButton icon={FaSolidPlus} onClick={() => {}} title="Add" />
