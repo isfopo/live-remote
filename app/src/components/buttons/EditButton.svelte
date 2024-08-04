821d222f66169c1e8ce0048a89aada6243cb05b8
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-03 23:52:32 -0400

    ✨ (EditButton.svelte): refactor button click logic to use a separate `toggle` function for better readability and maintainability

--- app/src/components/buttons/EditButton.svelte
+++ app/src/components/buttons/EditButton.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-  import { state } from "../../state";
+  import { state, type State } from "../../state";
   import IconButton from "./IconButton.svelte";
   import { FaSolidPen } from "svelte-icons-pack/fa";
+
+  const toggle = () => {
+    state.update(
+      (state): State => ({
+        ...state,
+        grid: {
+          ...state.grid,
+          editing: !state.grid.editing,
+        },
+      })
+    );
+  };
 </script>
 
-<IconButton
-  icon={FaSolidPen}
-  onClick={() => {
-    state.update((state) => ({
-      ...state,
-      editing: {
-        ...state.grid,
-        editing: !state.grid.editing,
-      },
-    }));
-  }}
-  title="Edit"
-/>
+<IconButton icon={FaSolidPen} onClick={toggle} title="Edit" />
