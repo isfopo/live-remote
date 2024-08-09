b15e91fa4cce76f12abbef2f55362da003289dab
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-08-09 19:45:12 -0400

    ✨ (IconButton.svelte): Add tooltip functionality to IconButton component available on hover or focus to improve user experience

--- app/src/components/buttons/IconButton.svelte
+++ app/src/components/buttons/IconButton.svelte
@@ -9,18 +9,52 @@
   export let color: ThemeColors = "onPrimaryContainer";
   export let onClick: () => void;
 
+  let showTooltip = false;
   let _color: string = $state.theme.get(color);
 
   $: {
     _color = $state.theme.get(color);
   }
+
+  const handleMouseEnter = () => {
+    showTooltip = true;
+  };
+
+  const handleMouseLeave = () => {
+    showTooltip = false;
+  };
 </script>
 
-<button on:click={onClick} {title}>
-  <Icon src={icon} {size} color={_color} />
-</button>
+<div
+  class="button-container"
+  role="button"
+  tabindex="0"
+  on:mouseenter={handleMouseEnter}
+  on:mouseleave={handleMouseLeave}
+  on:focus={handleMouseEnter}
+  on:blur={handleMouseLeave}
+  on:keydown={(e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      onClick();
+    }
+  }}
+>
+  <button on:click={onClick} {title}>
+    <Icon src={icon} {size} color={_color} />
+  </button>
+
+  {#if showTooltip}
+    <div role="tooltip" class="tooltip">
+      {title}
+    </div>
+  {/if}
+</div>
 
 <style>
+  .button-container {
+    position: relative;
+    display: inline-block;
+  }
   button {
     border: none;
     background: none;
@@ -35,5 +69,18 @@
     outline: none;
     background-color: transparent;
     transform: scale(1.1);
+  }
+
+  .tooltip {
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    background-color: var(--theme-surface);
+    color: var(--theme-onSurface);
+    border-radius: 4px;
+    padding: 5px;
+    white-space: nowrap;
+    z-index: 10;
   }
 </style>
