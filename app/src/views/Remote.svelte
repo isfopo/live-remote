c1811f54caa9dec6188959aaa426ebf79fecac6d
Isaac Poole <isaacpoolemusic@gmail.com>
2025-08-22 23:26:44 -0400

    Refactor Remote view

--- app/src/views/Remote.svelte
+++ app/src/views/Remote.svelte
@@ -1,37 +1,35 @@
 <script lang="ts">
-  import { widgets, type WidgetOnGrid } from "../widgets";
-  import { state } from "../state";
-  import DialogContainer from "../components/dialogs/DialogContainer.svelte";
-  import { Grid, GridItem, type LayoutChangeDetail } from "../components/grid";
+    import { widgets, type WidgetOnGrid } from "../widgets";
+    import { state } from "../state";
+    import DialogContainer from "../components/dialogs/DialogContainer.svelte";
+    import {
+        Grid,
+        GridItem,
+        type LayoutChangeDetail,
+    } from "../components/grid";
 
-  const itemSize = { height: 40 };
+    const itemSize = { height: 40 };
 
-  const change = (e: CustomEvent<LayoutChangeDetail>): void => {
-    $state.grid.update(e.detail.item as WidgetOnGrid);
-  };
+    const change = (e: CustomEvent<LayoutChangeDetail>): void => {
+        $state.grid.update(e.detail.item as WidgetOnGrid);
+    };
 </script>
 
 <Grid
-  {itemSize}
-  cols={10}
-  collision="push"
-  on:change={change}
-  readOnly={!$state.grid.editing}
+    {itemSize}
+    cols={10}
+    collision="push"
+    on:change={change}
+    readOnly={!$state.grid.editing}
 >
-  {#each $state.grid.items as { id, x, y, w, h }}
-    <GridItem {id} {x} {y} {w} {h} resizable={false}>
-      <svelte:component this={widgets[id]?.component} />
-    </GridItem>
-  {/each}
+    {#each $state.grid.items as { id, x, y, w, h }}
+        <GridItem {id} {x} {y} {w} {h} resizable={false}>
+            <svelte:component this={widgets[id]?.component} />
+        </GridItem>
+    {/each}
 </Grid>
 
 <DialogContainer />
 
 <style>
-  .container {
-    width: 100%;
-    height: 100%;
-    padding: 1rem;
-    box-sizing: border-box;
-  }
 </style>
